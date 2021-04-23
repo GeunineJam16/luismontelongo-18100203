2144f39f48669a58ba7bd2eb574bc5e692b8f483
GeunineJam16 <57452636+GeunineJam16@users.noreply.github.com>
2021-04-23 12:04:09 -0500

    Add files via upload

---
+++ Login/login.js
@@ -0,0 +1,12 @@
+$("#btnlogear").click(function(){
+
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Inciando session',
+        showConfirmButton: false,
+        timer: 1500
+      });
+
+});
+

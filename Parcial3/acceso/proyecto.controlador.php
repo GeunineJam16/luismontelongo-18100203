5858e9e8a766fd045eb15029ef2eb74bcdc04077
GeunineJam16 <57452636+GeunineJam16@users.noreply.github.com>
2021-06-11 12:42:02 -0500

    Update proyecto.controlador.php

--- Parcial3/acceso/proyecto.controlador.php
+++ Parcial3/acceso/proyecto.controlador.php
@@ -8,7 +8,47 @@ class ControladorProyecto{
 		
 		$respuesta = ModeloProyecto::mdlValidarSession($User,$Pass);
 
-		return json_encode($respuesta);
+		return $respuesta;
+	}
+
+	static public function ctrDataGridArticulos(){
+		
+		$respuesta = ModeloProyecto::mdlDataGridArticulos();
+
+		foreach ($respuesta as $row) {
+
+			$output[] = array(
+
+				'id' => $row['id'],
+				'Descripcion' => $row['Descripcion'],
+				'Marca' => $row['Marca'],
+				'UM' => $row['UM'],
+				'Proveedor' => $row['Proveedor'],
+				'TipoArticulo' => $row['TipoArticulo']
+			
+			);
+
+		}
+
+		return json_encode($output);
+	}
+
+	//guardado de la infomacion de articulos
+	static public function ctrGuardarArticulo($CodArticulo,$NombreArti,$MarcaArticulo,$selectUM,$selectProveedores,$selectTipodeArticulo){
+		
+		$respuesta = ModeloProyecto::mdlGuardarArticulo($CodArticulo,$NombreArti,$MarcaArticulo,$selectUM,$selectProveedores,$selectTipodeArticulo);
+
+
+		return $respuesta;
+	}
+
+	//Eliminacion de la infomacion de articulos
+	static public function ctrDataGridArticulosEliminar($query){
+		
+		$respuesta = ModeloProyecto::DataGridArticulosEliminar($query);
+
+		var_dump($respuesta);
+		return $respuesta;
 	}
 
 
